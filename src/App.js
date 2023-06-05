eb343be4f5934afde438c8942b8f0c2cc5e2d969
Joseph Noblett <noblettjoe@gmail.com>
2023-06-05 15:32:39 -0500

    set char's useState & useEffect/axiosGet to make a api request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ReactDOM } from 'react';
+import axios from 'axios';
+import Characters from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,6 +10,18 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [characters, setCharacters] = useState([])
+  
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        setCharacters(res.data)})
+      .catch(err => {
+        console.log(err)})  
+  }, [])
+
 
   return (
     <div className="App">
