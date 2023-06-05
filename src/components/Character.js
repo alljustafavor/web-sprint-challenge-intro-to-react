f37cf2800e10451466e3ef63d51f414ff9c2e9d6
Joseph Noblett <noblettjoe@gmail.com>
2023-06-05 15:33:27 -0500

    Basic init commit of the child characters.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
-// Write your Character component here
+import React, { useState, useEffect } from 'react';
+import { ReactDOM } from 'react';
+import axios from 'axios';
+ 
+
+const Characters = ({ props }) => {
+
+}
+
+export default Characters
